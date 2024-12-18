e22d51d8aa488cf9227c3f610020ca875baf050b
DB7YI Michael <db7yi@darc.de>
2024-12-18 20:20:03 +0100

    i2c address

--- Si5351.cpp
+++ Si5351.cpp
@@ -1,5 +1,24 @@
 #include <Si5351.hpp>
 
-Si5351::Si5351(i2c_inst i2cPort, uint8_t sda, uint8_t scl, double xtalFreq)
-:I2C_PORT(i2cPort), SDA(sda), SCL(scl), XTAL_FREQ(xtalFreq)
-{}
+void Si5351::waitAfterPowerOn()
+{
+    uint8_t siRegister = 0;
+    uint8_t siData;
+    do
+    {
+        i2c_write_blocking(I2C_PORT, I2C_ADDR, &siRegister, 1, false);
+        i2c_read_blocking(I2C_PORT, I2C_ADDR, &siData, 1, false);
+    } while ((siData & 0x88) != 0);
+}
+
+Si5351::Si5351(i2c_inst i2cPort, uint8_t i2cAddr, uint8_t sda, uint8_t scl, double xtalFreq)
+    : I2C_PORT(I2C_PORT), I2C_ADDR(i2cAddr), SDA(sda), SCL(scl), XTAL_FREQ(xtalFreq)
+{
+    waitAfterPowerOn();
+}
+
+void Si5351::outputsOff()
+{
+    uint8_t allOutputsOff[2] = {3, 0xFF};
+    i2c_write_blocking(I2C_PORT, I2C_ADDR, allOutputsOff, 2, false);
+}
--- Si5351.hpp
+++ Si5351.hpp
@@ -5,17 +5,29 @@
 class Si5351
 {
 private:
-    const i2c_inst I2C_PORT;
+    i2c_inst* I2C_PORT;
+    const uint8_t I2C_ADDR;
     const uint8_t SDA;
     const uint8_t SCL; 
     const double XTAL_FREQ;
 
+    /**
+     * @ brief Blocks until Si5351 is intialized and CLKIN is valid
+     */
+    void waitAfterPowerOn();
+
 public:
     /**
      * @param i2cPort is an I2C controller, for example `i2c0`. It must have been initialized.
+     * @param i2cAddr is the I2C address.
      * @param sda is a GPIO pin that can be used as SDA with `i2cPort`, for example `0`.
      * @param scl is a GPIO pin that can be used as SCL with `i2cPort`, for example `1`.
      * @param xtalFreq is the crystal frequency in Hz.
      */
-    Si5351(i2c_inst i2cPort, uint8_t sda, uint8_t scl, double xtalFreq);
+    Si5351(i2c_inst i2cPort, uint8_t i2cAddr, uint8_t sda, uint8_t scl, double xtalFreq);
+
+    /**
+     * @brief Disables all output drivers and powers them down.
+     */
+    void outputsOff()
 };
